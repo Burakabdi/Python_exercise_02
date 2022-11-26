b2ec49a8051e0a1bd7e6738ee3429638ef686165
Burakabdi <burakabdiolu@gmail.com>
2022-11-26 17:06:28 +0300

    exc_15

---
+++ python_excersice_self/exc_15.py
@@ -0,0 +1,10 @@
+# Write a Python function to calculate the factorial of a number (a non-negative integer). The function accepts the number as an argument..
+
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n* fact(n-1)
+
+n = int(input("enter a number to calculate the factorial: "))
+print(fact(n))
