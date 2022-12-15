c30a95459b4d878f041e9dccb62ad30efb81f516
Burakabdi <burakabdiolu@gmail.com>
2022-12-15 20:36:52 +0300

    exc_21

--- python_excersice_self/exc_19.py
+++ python_excersice_self/exc_19.py
@@ -1,5 +1,5 @@
 # Write a Python function that takes a number as a parameter and check the number is prime or not. 
-# Note : A prime number (or a prime) is a natural number greater than 1 and that has no positive divisors other than 1 and itself.
+# Note : A prime number is a natural number greater than 1 and that has no positive divisors other than 1 and itself.
 
 
 def test_prime(number):
@@ -11,7 +11,7 @@ def test_prime(number):
         for x in range(2,number):
             if(number % x==0):
                 return False
-        return True             
-print(test_prime(7))
-        
+        return True        
 
+     
+print(test_prime(7))
---
+++ python_excersice_self/exc_21.py
@@ -0,0 +1,29 @@
+# Write a Python program to calculate a dog's age in dog's years.
+# Note: For the first two years, a dog year is equal to 10.5 human years. After that, each dog year equals 4 human years.
+# Expected Output:
+
+# Input a dog's age in human years: 15                                    
+# The dog's age in dog's years is 73
+
+
+dog_age_in_human = int(input("Input a dog's age in human years: "))
+
+# def dog_age_finder(dog_age_in_human):
+#     without_first_two_years = dog_age_in_human-2
+#     final_age_in_human = (without_first_two_years*4) + 21
+#     print(final_age_in_human)
+
+# dog_age_finder(15)
+
+if dog_age_in_human < 0:
+    print("dog age must be positive!")
+    exit()
+elif dog_age_in_human <= 2:
+    dog_age_in_dog = dog_age_in_human*10.5
+else:
+    dog_age_in_dog = 21 + (dog_age_in_human-2)*4
+    
+print("The dog's age in dog's years is", dog_age_in_dog)
+
+
+
